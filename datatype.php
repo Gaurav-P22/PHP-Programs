9e0d8a5a55b16eb0c364ca1ecbcd057a31a0226f
Gaurav Prajapati <118986007+GauravKiet@users.noreply.github.com>
2023-02-01 22:11:41 +0530

    Add files via upload

---
+++ datatype.php
@@ -0,0 +1,18 @@
+<?php
+//Program to demonstration of PHP Data Types
+$a=10;
+$b=20.5;
+$c='a';
+$d="Analyze";
+$e=true;
+
+echo"<br>a=".$a;
+echo"<br>b=".$b;
+echo"<br>c=".$c;
+echo"<br>d=".$d;
+echo"<br>e=".$e;
+echo"<hr>Hi ";
+echo'<p style="color:Orangered; font-style: Italic;">Lorem ipsum dolor sit amet consectetur adipisicing elit</p>';
+echo'<h1 style="color:Orangered; font-style: Italic;">Lorem ipsum dolor sit amet consectetur adipisicing elite</h1>';
+
+?>
