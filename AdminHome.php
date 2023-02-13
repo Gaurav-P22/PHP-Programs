3f47d14ef4b4049e0954c541b9adbc823d00fffe
Gaurav Prajapati <118986007+GauravKiet@users.noreply.github.com>
2023-02-13 22:04:27 +0530

    Add files via upload

---
+++ AdminHome.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+    </head>
+    <body bgcolor="lightyellow">
+        <h1 style="color: red; margin:auto; border:4px solid red; border-radius:20px; padding:15px;">This Is the Admin  Page</h1>
+        
+    </body>
+</html>
