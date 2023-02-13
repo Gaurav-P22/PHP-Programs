6e11222abc790479a9c1c2dbf82d63e69a293cbc
Gaurav Prajapati <118986007+GauravKiet@users.noreply.github.com>
2023-02-13 22:04:52 +0530

    Add files via upload

---
+++ EmpInfo.php
@@ -0,0 +1,49 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        <center>
+            <h1>
+                Employee Registration Info
+            </h1>
+            <form action="EmpRegConf.php">
+                <table width="800" border="1" cellspacing="0" bgcolor="lightyellow" height="700" cellpadding="20">
+                    <tr>
+                        <td width="400">Employee Code</td>
+                        <td><input type="number" name="code"></td>
+                    </tr>
+                    <tr>
+                        <td width="400">Name of the Employee</td>
+                        <td><input type="text" name="name"></td>
+                    </tr>
+                    <tr>
+                        <td width="400">Employee Post: </td>
+                        <td><input type="text" name="post"></td>
+                    </tr>
+                    <tr>
+                        <td width="400">Employee Address: </td>
+                        <td><input type="text" name="addr"></td>
+                    </tr>
+                    <tr>
+                        <td width="400">Employee city: </td>
+                        <td><input type="text" name="city"></td>
+                    </tr>
+                    <tr>
+                        <td width="400">Employee Contact: </td>
+                        <td><input type="number" name="cont"></td>
+                    </tr>
+                    <tr>
+                        <td width="400">Employee Email: </td>
+                        <td><input type="mail" name="mail"></td>
+                    </tr>
+                    <tr>
+                        <td width="400"> <input type="submit" value="Submit"></td>
+                        <td><input type="Reset" value="Reset"></td>
+                    </tr>
+
+                </table>
+            </form>
+        </center>
+    </body>
+</html>
